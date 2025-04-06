345712f06b74320a193873ecba184dbd32be356d
Apekun Dmitry <zasferg@gmail.com>
2025-04-06 17:04:05 +0300

    [feat]:added_docker_doc_string_service_layer

--- part_3/src/ed_project/api/validators.py
+++ part_3/src/ed_project/api/validators.py
@@ -1,12 +1,28 @@
 from django.core.exceptions import ValidationError
 
-
 def range_validate(value):
+    """
+    Валидатор для проверки диапазона значений.
+
+    Args:
+        value (int): Значение для проверки.
+
+    Raises:
+        ValidationError: Если значение не находится в диапазоне от 1 до 5.
+    """
     if value < 1 or value > 5:
         raise ValidationError("Поле должно быть от 1 до 5")
 
-
 def friendliness_text_validate(value):
+    """
+    Валидатор для проверки текста дружелюбности.
+
+    Args:
+        value (str): Значение для проверки.
+
+    Raises:
+        ValidationError: Если значение не является одним из допустимых вариантов.
+    """
     if value not in ["Tiny", "Small", "Medium", "Large"]:
         raise ValidationError(
             "Поле должно быть только 'Tiny', 'Small', 'Medium', 'Large'"
